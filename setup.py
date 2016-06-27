f6bd8e0dfbc295f968d0034988ff166b00c29a85
PalomaCiffBigData <palomafernandez@campusciff.net>
2016-06-27 20:16:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ciffmbd2016pfdg',
-      version='0.4',  # Development release
+      version='0.8',  # Development release
       description='',
       url='https://github.com/PalomaCiffBigData/Model_P_y_D',
       author='Paloma Fdez y Daniel Gibaja',
